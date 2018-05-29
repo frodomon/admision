0dec49c77b1076d24d85a3e3ca06c503d54a743b
Alfredo <vasquez.alf@gmail.com>
2018-05-28 19:00:08 -0500

    Date Format

--- app/controllers/static_pages_controller.rb
+++ app/controllers/static_pages_controller.rb
@@ -7,7 +7,7 @@ class StaticPagesController < ApplicationController
   end
   def index
   	if user_signed_in?
-  	  redirect_to dashboard_path
+  	  redirect_to requests_path
   	else
   	  redirect_to new_user_session_path
   	end
