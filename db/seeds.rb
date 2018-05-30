35f6e736c831ca5b86c65708e94376b22d955e23
Alfredo <vasquez.alf@gmail.com>
2018-05-30 10:42:56 -0500

    seed apj

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,3 +1,3 @@
 
-User.create(name:'Jorge', last_name: 'Yazawa', agencia: 'Principal',  email: 'jyazawa@aelucoop.com.pe', password: 'aelucoop', password_confirmation: 'aelucoop', roles_mask: 8)
-User.create(name:'Javier', last_name: 'Miyashiro', agencia: 'Principal',  email: 'jmiyashiro@aelucoop.com.pe', password: 'aelucoop', password_confirmation: 'aelucoop', roles_mask: 8)
+User.create(name:'Jorge', last_name: 'Yazawa', email: 'jyazawa@aelucoop.com.pe', password: 'aelucoop', password_confirmation: 'aelucoop', roles_mask: 8)
+User.create(name:'Javier', last_name: 'Miyashiro', email: 'jmiyashiro@aelucoop.com.pe', password: 'aelucoop', password_confirmation: 'aelucoop', roles_mask: 8)
