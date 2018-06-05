b88d2566801231fb05de43e75cf14d954ee3022d
Alfredo <vasquez.alf@gmail.com>
2018-06-05 12:36:49 -0500

    campos en observacion

--- app/controllers/observations_controller.rb
+++ app/controllers/observations_controller.rb
@@ -72,6 +72,6 @@ class ObservationsController < ApplicationController
 
     # Never trust parameters from the scary internet, only allow the white list through.
     def observation_params
-      params.require(:observation).permit(:request_id, :observation_type_id, :description)
+      params.require(:observation).permit(:request_id, :observation_type_id, :description, :farakaki, :wsanchez, :cchinen, :akira)
     end
 end
